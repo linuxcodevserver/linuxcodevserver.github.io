a2e9b03652dcbf1f7118715054a18e31ae2326ae
okaits7534 <okaits7534@gmail.com>
2021-10-20 21:17:05 +0900

    [add] ありがとうページ作成

--- docs/javascript/mp-donatemona/mp-donatemona05.js
+++ docs/javascript/mp-donatemona/mp-donatemona05.js
@@ -1,2 +1,9 @@
 const txHash = window.mpurse.sendAsset('MB56hAzDE9s7KeCmnPfFk7qVe77tq57ZPa', 'MONA', 5, 'plain','test');
 console.log("txHash: txHash")
+var txdiv = document.getElementById('txid')
+var txid = document.createElement('p');
+txid.textContent = 'ID: ' + txHash;
+var stxid = document.createElement('p');
+stxid.textContent = 'IDはコピーしておいてください。';
+txdiv.appendChild(txid);
+txdiv.appendChild(stxid);

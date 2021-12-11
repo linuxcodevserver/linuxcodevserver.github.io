4d3feeb5c5202767ef2e490e286872daf873a4dc
okaits <okaits7534@gmail.com>
2021-12-11 12:00:51 +0900

    [add] 広告追加&テスト

---
+++ docs/javascript/adservice/main.js
@@ -0,0 +1,14 @@
+/* Settings */
+/* Set class name */
+const adssp = document.getElementsByClassName('adservice-sp'); /* for SmartPhone */
+const adspc = document.getElementsByClassName('adservice-pc'); /* for PC */
+/* Set Adservice Code */
+const invidsp=42;
+const invidpc=57;
+
+/* Main Code */
+if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+    adssp.innerHTML = '<script src="https://aas.information-portal.net/tag.php?invid=' + invidsp + '"></script>';
+} else {
+    adspc.innerHTML = '<script src="https://aas.information-portal.net/tag.php?invid=' + invidpc + '"></script>';
+}

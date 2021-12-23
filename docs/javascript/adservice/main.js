847c7fef5da12514c21ef51c06450de9391b3f5e
okaits <okaits7534@gmail.com>
2021-12-23 19:03:24 +0900

    [fix] 文法エラー修正

--- docs/javascript/adservice/main.js
+++ docs/javascript/adservice/main.js
@@ -2,8 +2,6 @@
 /* Set class name */
 const adssp = document.getElementsByClassName('adservice-sp'); /* for SmartPhone */
 const adspc = document.getElementsByClassName('adservice-pc'); /* for PC */
-/* Create new Element */
-var adstag = document.createElement('script');
 /* Set Adservice Code */
 const invidsp=42;
 const invidpc=57;
@@ -11,10 +9,16 @@ const invidpc=57;
 /* Main Code */
 if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
 //    adssp.innerHTML = '<script src="https://aas.information-portal.net/tag.php?invid=' + invidsp + '"></script>';  // createElementへの移行により廃止
-    adstag.setAttribute("src", "https://aas.information-potal.net/tag.php?invid=" + invidsp);
-    adssp.appendChild(adstag)
+    for (i = 0; i < adssp.length; i++) {
+        var adstag = document.createElement();
+        adstag.setAttribute("src", "https://aas.information-potal.net/tag.php?invid=" + invidsp);
+        var adstag = adssp[i].appendChild(adstag);
+    }
 } else {
 //    adspc.innerHTML = '<script src="https://aas.information-portal.net/tag.php?invid=' + invidpc + '"></script>'; // createElementへの移行により廃止
-    adstag.setAttribute("src", "https://aas.information-potal.net/tag.php?invid=" + invidpc);
-    adspc.appendChild(adstag);
+    for (i = 0; i < adspc.length; i++) {
+        var adstag = document.createElement();
+        adstag.setAttribute("src", "https://aas.information-potal.net/tag.php?invid=" + invidpc);
+        var adstag = adspc[i].appendChild(adstag);
+    }
 }

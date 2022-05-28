418cf6e307e61a7ae5050ac8dd49c7d3902a458c
okaits7534 <okaits7534@gmail.com>
2022-05-28 12:24:22 +0900

    [update] 寄付ページ更新

---
+++ docs/javascript/mp-donatemona/donate.js
@@ -0,0 +1,4 @@
+amount = window.prompt("寄付額を入力してください。")
+const txHash = window.mpurse.sendAsset('MB56hAzDE9s7KeCmnPfFk7qVe77tq57ZPa', 'MONA', amount, 'plain','test');
+console.log("txHash: " + txHash)
+window.location = "javascript/mp-donatemona/thanks.html?txhash=" + txHash + "&amount=" + amount

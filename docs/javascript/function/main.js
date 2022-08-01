6b935c1a9fc5106756e85c446755dcdab7acd7fe
okaits <okaits7534@gmail.com>
2022-08-01 12:06:49 +0900

    [fix] AdService広告が表示されない問題の修正 Fixes #36

--- docs/javascript/function/main.js
+++ docs/javascript/function/main.js
@@ -17,4 +17,28 @@ function insertcode_main() {
       wcgwidgetteamtag.contentWindow.close();
   }
 }
+
+function sleep(waitSec, callbackFunc) {
+ 
+  // 経過時間（秒）
+  var spanedSec = 0;
+
+  // 1秒間隔で無名関数を実行
+  var id = setInterval(function () {
+
+      spanedSec++;
+
+      // 経過時間 >= 待機時間の場合、待機終了。
+      if (spanedSec >= waitSec) {
+
+          // タイマー停止
+          clearInterval(id);
+
+          // 完了時、コールバック関数を実行
+          if (callbackFunc) callbackFunc();
+      }
+  }, 1000);
+
+}
 window.globalFunction.insertcode_main = insertcode_main;
+window.globalFunction.sleep = sleep;
